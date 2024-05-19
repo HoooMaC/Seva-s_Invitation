1152174bcb8145fd29982a2cf54910e60f94face
HoooMaC <wawcam123@gmail.com>
2024-05-19 21:28:29 +0700

    making family component for family card

--- src/pages/Family.jsx
+++ src/pages/Family.jsx
@@ -1,11 +1,127 @@
 import TopNavbar from "../components/TopNavbar";
-import ImageContainer from "../components/ImageContainer";
 
 import "./Family.css";
 
-import placehoderImage from "../assets/images/temp/PLACEHOLDER IMAGE.png";
+import placeholderImage from "../assets/images/temp/PLACEHOLDER IMAGE.png";
+import { AnimatePresence, motion } from "framer-motion";
+import React, { useRef, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faChevronLeft,
+  faChevronRight,
+} from "@fortawesome/free-solid-svg-icons";
+import Button from "../components/Button.jsx";
+
+const variantIn = {
+  initial: {
+    y: 0,
+    x: 100,
+    scale: 0.7,
+    opacity: 0,
+  },
+  animate: {
+    x: 0,
+    y: 0,
+    scale: 1,
+    opacity: 1,
+    transition: {
+      duration: 0.2,
+    },
+  },
+  exit: {
+    y: 0,
+    x: -100,
+    scale: 0.7,
+    opacity: 0,
+    transition: {
+      duration: 0.2,
+    },
+  },
+};
+
+const variantOut = {
+  initial: {
+    y: 0,
+    x: -100,
+    scale: 0.7,
+    opacity: 0,
+  },
+  animate: {
+    y: 0,
+    x: 0,
+    scale: 1,
+    opacity: 1,
+    transition: {
+      duration: 0.2,
+    },
+  },
+  exit: {
+    y: 0,
+    x: 100,
+    scale: 0.7,
+    opacity: 0,
+    transition: {
+      duration: 0.2,
+    },
+  },
+};
+
+const CarouselCard = ({ id, title, subtitle, placeholderImage, animate }) => {
+  return (
+    <motion.div
+      variants={animate ? variantIn : variantOut}
+      initial="initial"
+      animate="animate"
+      exit="exit"
+      id={id}
+      className="family-card"
+    >
+      <img src={placeholderImage} alt={title} className="family-image" />
+      <h1 className="family-name">{title}</h1>
+      <p className="family-subtitle">{subtitle}</p>
+    </motion.div>
+  );
+};
 
 const Family = () => {
+  const items = [
+    {
+      id: "1",
+      title: "Jhon",
+      subtitle: "Unset",
+      image: placeholderImage,
+    },
+    {
+      id: "2",
+      title: "Emma",
+      subtitle: "Unset",
+      image: placeholderImage,
+    },
+    {
+      id: "3",
+      title: "Doe",
+      subtitle: "Unset",
+      image: placeholderImage,
+    },
+    {
+      id: "4",
+      title: "Asep",
+      subtitle: "Unset",
+      image: placeholderImage,
+    },
+  ];
+  const [index, setIndex] = useState(0);
+  const [isForward, setIsForward] = useState();
+  const handleNext = () => {
+    setIsForward(true);
+    setIndex(prevIndex => (prevIndex + 1) % items.length);
+  };
+
+  const handlePrev = () => {
+    setIsForward(false);
+    setIndex(prevIndex => (prevIndex - 1 + items.length) % items.length);
+  };
+
   return (
     <>
       <TopNavbar className="fixed-top"></TopNavbar>
@@ -13,21 +129,26 @@ const Family = () => {
       <div className="board-title-container">
         <h1 className="board-title">KELUARGA</h1>
       </div>
-      <div className="section">
-        <div className="scrollable-container">
-          {/* <div className="overlay" /> */}
-          {/* <ImageContainer src={example} title="Mena Tiara" /> */}
-          <ImageContainer src={placehoderImage} title="UNDEFINED" />
-          <ImageContainer src={placehoderImage} title="UNDEFINED" />
-          <ImageContainer src={placehoderImage} title="UNDEFINED" />
-          <ImageContainer src={placehoderImage} title="UNDEFINED" />
-          <ImageContainer src={placehoderImage} title="UNDEFINED" />
-          <ImageContainer src={placehoderImage} title="UNDEFINED" />
-          <ImageContainer src={placehoderImage} title="UNDEFINED" />
-          <ImageContainer src={placehoderImage} title="UNDEFINED" />
-          <ImageContainer src={placehoderImage} title="UNDEFINED" />
-          <ImageContainer src={placehoderImage} title="UNDEFINED" />
+
+      <div className="family-section">
+        <div className="carousel-navigator-container">
+          <Button className="circle-button" onClick={handlePrev}>
+            <FontAwesomeIcon icon={faChevronLeft} />
+          </Button>
+          <Button className="circle-button" onClick={handleNext}>
+            <FontAwesomeIcon icon={faChevronRight} />
+          </Button>
         </div>
+        <AnimatePresence initial={false} mode={"wait"}>
+          <CarouselCard
+            key={items[index].id}
+            id={items[index].id}
+            title={items[index].title}
+            subtitle={items[index].subtitle}
+            placeholderImage={items[index].image}
+            animate={isForward}
+          />
+        </AnimatePresence>
       </div>
     </>
   );
